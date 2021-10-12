54fb0d41079a617e4a5ddc49030fb256cc00c737
David <DEvangelista54@gmail.com>
2021-10-12 12:50:11 -0400

    commit tree 3909b89d74cff3c23d7e5f2efae6b7e1b8493639

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+    puts "Hello, my name is #{name}."
+    "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+    array.map do |x|
+        "Hello, my name is #{x}."
+    end
+end
+
+def assign_rooms(array)
+    array.map.with_index(1) do |x, room_num|
+        "Hello, #{x}! You'll be assigned to room #{room_num}!"
+    end
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |result|
+      puts result
+    end
+
+    assign_rooms(array).each do |result|
+        puts result
+    end
+
+end
+
+printer(["John", "Jesse", "Dundee", "Yuno", "Benji"])
